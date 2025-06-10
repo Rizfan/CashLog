5246965c3394eefab10735fdb300dfdd121cf6d1
Rizfan <rispanherlaya@gmail.com>
2025-06-10 23:11:50 +0700

    fix: reorder user route definition for clarity in API routes

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,16 @@
 use App\Http\Controllers\Api\BudgetController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Exceptions;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\UnauthorizedException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('budgets', BudgetController::class);
 });
